cb0aee19ab8a85051fe546f9516968df78b41e42
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-05 15:50:25 +0200

    adding login route

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id='nav'>
+      <router-link to='/'>Home</router-link> |
+      <router-link to='/about'>About</router-link> |
+      <router-link to='/login'>Login</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-@import "./styles/_variables.scss";
-@import "./styles/styles.scss";
-
-#app {
-  color: $primary;
-}
+<style lang='scss'>
+@import './styles/_variables.scss';
+@import './styles/styles.scss';
 
 #nav {
   padding: 30px;
