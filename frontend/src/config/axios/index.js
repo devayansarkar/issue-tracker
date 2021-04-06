7315f5c7d723fdc23092fb7cebb9da5d28d0fea2
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 21:59:04 +0200

    suing Bearer token based auth

--- frontend/src/config/axios/index.js
+++ frontend/src/config/axios/index.js
@@ -18,26 +18,23 @@ const openConnection = axios.create({
 
 securedConnection.interceptors.request.use((config) => {
   // eslint-disable-next-line no-param-reassign
-  config.headers = {
-    ...config.headers,
-    'X-CSRF-TOKEN': localStorage.csrf,
-  };
+  config.headers.Authorization = `Bearer ${localStorage.tokens.access}`;
   return config;
 });
 
 securedConnection.interceptors.response.use(null, (error) => {
   if (error.respons && error.response.config && error.response.status === 401) {
-    return openConnection.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
+    return openConnection.post('/refresh', {}, { headers: { Authorization: `Bearer ${localStorage.token.refresh}` } })
       .then((response) => {
-        localStorage.csrf = response.data.csrf;
+        localStorage.tokens = response.data;
         localStorage.signedIn = true;
 
         const retryConfig = error.response.config;
-        retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf;
+        retryConfig.headers.Authorization = `Bearer ${localStorage.tokens.access}`;
         return openConnection.request(retryConfig);
       })
       .catch((exception) => {
-        delete localStorage.csrf;
+        delete localStorage.tokens;
         delete localStorage.signedIn;
 
         // eslint-disable-next-line no-restricted-globals
