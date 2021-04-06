4ce4692d95b57a587fa10f0f6b32f138b4084fef
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 21:29:03 +0200

    adding colons

--- frontend/src/config/axios/index.js
+++ frontend/src/config/axios/index.js
@@ -7,7 +7,7 @@ const securedConnection = axios.create({
     headers: {
         'Content-Type': 'application/json'
     }
-})
+});
 
 const openConnection = axios.create({
     baseURL: '/',
@@ -15,37 +15,37 @@ const openConnection = axios.create({
     headers: {
         'Content-Type': 'application/json'
     }
-})
+});
 
 securedConnection.interceptors.request.use(config => {
     config.headers = {
         ...config.headers,
         'X-CSRF-TOKEN': localStorage.csrf
     }
-    return config
-})
+    return config;
+});
 
 securedConnection.interceptors.response.use(null, error => {
     if (error.respons && error.response.config && error.response.status === 401) {
         return openConnection.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
             .then(response => {
-                localStorage.csrf = response.data.csrf
-                localStorage.signedIn = true
+                localStorage.csrf = response.data.csrf;
+                localStorage.signedIn = true;
 
-                let retryConfig = error.response.config
-                retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
-                return openConnection.request(retryConfig)
+                let retryConfig = error.response.config;
+                retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf;
+                return openConnection.request(retryConfig);
             })
             .catch(error => {
-                delete localStorage.csrf
-                delete localStorage.signedIn
+                delete localStorage.csrf;
+                delete localStorage.signedIn;
 
-                location.replace('/')
-                return Promise.reject(error)
+                location.replace('/');
+                return Promise.reject(error);
             })
     } else {
-        return Promise.reject(error)
+        return Promise.reject(error);
     }
-})
+});
 
 export { securedConnection, openConnection }
