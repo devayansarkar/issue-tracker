d98d8b3e87302c3f2060ee667b40ec9be83f6795
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 21:34:40 +0200

    configuring Vue Axios for the app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { securedConnection, openConnection } from './config/axios';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(VueAxios, { secured: securedConnection, plain: openConnection })
+  .use(router)
+  .mount('#app');
