492028be01648630fcd629b860a36070eeb4261c
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 18:39:32 +0200

    common methods

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+    include JWTSessions::RailsAuthorization
+    rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+    def action_not_found
+        render json:{error: 'The resource you are requesting is not available.'}, status: :not_found
+    end
+    private 
+
+    def current_user
+        @current_user ||= User.find(paylod['user_id'])
+    end
+
+    def not_authorized
+        render json:{error: 'Access denied. You must be logged in to access this resource.'}, status: :unauthorized
+    end
+
 end
