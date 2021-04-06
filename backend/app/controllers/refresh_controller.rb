05a89c03e7445e0c2786289f2ac68d270491cf34
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 21:52:24 +0200

    for allowing refreshing of tokens

--- backend/app/controllers/refresh_controller.rb
+++ backend/app/controllers/refresh_controller.rb
@@ -1,5 +1,5 @@
 class RefreshController < ApplicationController
-    before_action :authorize_refresh_by_access_request!
+    before_action :authorize_refresh_request!
 
     def create
         session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
