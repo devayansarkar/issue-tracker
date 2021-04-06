e3b248d76694373b6140dfd05082d50756748772
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 22:38:34 +0200

    fixing refresh controller for access tokens

--- backend/app/controllers/refresh_controller.rb
+++ backend/app/controllers/refresh_controller.rb
@@ -1,16 +1,17 @@
 class RefreshController < ApplicationController
-    before_action :authorize_refresh_request!
+    before_action :authorize_refresh_by_access_request!
 
-    def create
-        session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
-        tokens = session.refresh_by_access_allowed do
-            raise JWTSession::Errors:Unauthorized, "Not allowed"
-        end
-        response.set_cookie(JWTSessions.access_cookie,
-            value: tokens[:access],
-            httponly: true,
-            secure: Rails.env.production?)
+  def create
+    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    tokens = session.refresh_by_access_payload do
+       render json:{ error: 'Refresh action is performed before the expiration of the access token.'}, status: :bad_request
+       return
+      end
+    response.set_cookie(JWTSessions.access_cookie,
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
 
-        render json: { csrf: tokens[:csrf] }
-    end
+    render json: tokens
+  end
 end
