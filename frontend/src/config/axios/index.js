d79c06d2f6fc03a8eca9f89f300d13d8e159f381
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 21:31:59 +0200

    lint issues

--- frontend/src/config/axios/index.js
+++ frontend/src/config/axios/index.js
@@ -1,51 +1,51 @@
 import axios from 'axios';
 
-
 const securedConnection = axios.create({
-    baseURL: '/',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: '/',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 const openConnection = axios.create({
-    baseURL: '/',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: '/',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-securedConnection.interceptors.request.use(config => {
-    config.headers = {
-        ...config.headers,
-        'X-CSRF-TOKEN': localStorage.csrf
-    }
-    return config;
+securedConnection.interceptors.request.use((config) => {
+  // eslint-disable-next-line no-param-reassign
+  config.headers = {
+    ...config.headers,
+    'X-CSRF-TOKEN': localStorage.csrf,
+  };
+  return config;
 });
 
-securedConnection.interceptors.response.use(null, error => {
-    if (error.respons && error.response.config && error.response.status === 401) {
-        return openConnection.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
-            .then(response => {
-                localStorage.csrf = response.data.csrf;
-                localStorage.signedIn = true;
+securedConnection.interceptors.response.use(null, (error) => {
+  if (error.respons && error.response.config && error.response.status === 401) {
+    return openConnection.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
+      .then((response) => {
+        localStorage.csrf = response.data.csrf;
+        localStorage.signedIn = true;
 
-                let retryConfig = error.response.config;
-                retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf;
-                return openConnection.request(retryConfig);
-            })
-            .catch(error => {
-                delete localStorage.csrf;
-                delete localStorage.signedIn;
+        const retryConfig = error.response.config;
+        retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf;
+        return openConnection.request(retryConfig);
+      })
+      .catch((exception) => {
+        delete localStorage.csrf;
+        delete localStorage.signedIn;
 
-                location.replace('/');
-                return Promise.reject(error);
-            })
-    } else {
-        return Promise.reject(error);
-    }
+        // eslint-disable-next-line no-restricted-globals
+        location.replace('/');
+        return Promise.reject(exception);
+      });
+  }
+  return Promise.reject(error);
 });
 
-export { securedConnection, openConnection }
+export { securedConnection, openConnection };
