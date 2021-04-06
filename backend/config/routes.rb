5c6d61e4d725de76d70899ed0d9b51b6c489d20d
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-06 18:46:01 +0200

    Handling 404s and frontend views

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     post 'signup', controller: :signup, action: :create
     delete 'signin',controller: :signin, action: :destroy
 
+    # Only get requests return the html for the frontend
+    match '*path' => redirect(path: '/'), via: :get
+    # All other requests return 404s
     match '*path', to: 'application#action_not_found', via: :all
 end
