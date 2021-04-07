8cd5041984d391000109006037a9bcd14c458a6e
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-07 23:09:24 +0200

    user_id is added

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
     private 
 
     def current_user
-        @current_user ||= User.find(payload['id'])
+        @current_user ||= User.find(payload["user_id"])
     end
 
     def not_authorized
