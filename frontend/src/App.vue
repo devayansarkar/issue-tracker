54d29dabcc4858cc8f94980a280268cb216137bf
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-07 09:37:57 +0200

    just so the web page loads first

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
+    <router-view />
     <div id='nav'>
       <router-link to='/'>Home</router-link> |
       <router-link to='/about'>About</router-link> |
       <router-link to='/login'>Login</router-link> |
       <router-link to='/signup'>Sign up</router-link>
     </div>
-    <router-view />
   </div>
 </template>
 
