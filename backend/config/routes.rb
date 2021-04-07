8aca130c510e8f7ddc40800d74ab8f170e9bce3d
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-07 20:01:51 +0200

    exposing the issues resource

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+    namespace :api do
+        namespace :v1 do
+          resources :issues
+        end
+    end
+    
     post 'refresh', controller: :refresh, action: :create
     post 'signin', controller: :signin, action: :create
     post 'signup', controller: :signup, action: :create
