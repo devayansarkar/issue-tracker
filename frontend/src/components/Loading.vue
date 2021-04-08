059ace74d21bef0007c9ce708145ee060da594fb
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-08 19:52:57 +0200

    Loading component

---
+++ frontend/src/components/Loading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="water"></div>
+    <div class="loading">Loading...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+
+<style lang="scss">
+@import '../styles/loading.styles.scss';
+</style>
