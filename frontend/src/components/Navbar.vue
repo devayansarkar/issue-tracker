2cf7ff768b1729ed537eb9be953615c4f4fa58ac
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-08 18:45:34 +0200

    adding navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="navbar">
+    <div class="item">
+      <div class="icon logo">
+        <span class="material-icons"> group_work </span>
+      </div>
+    </div>
+    <div class="item">
+      <div>
+        <div class="icon">
+          <span class="material-icons"> home </span>
+        </div>
+        <div class="text">Home</div>
+      </div>
+    </div>
+    <div class="item">
+      <div>
+        <div class="icon">
+          <span class="material-icons"> assignment </span>
+        </div>
+        <div class="text">Tasks</div>
+      </div>
+    </div>
+    <div class="item active">
+      <div>
+        <div class="icon">
+          <span class="material-icons"> add_circle </span>
+        </div>
+        <div class="text">New task</div>
+      </div>
+    </div>
+    <div class="spacer"></div>
+    <div class="item">
+      <div class="icon">
+        <span class="material-icons"> info </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style lang="scss">
+@import '../styles/navbar.styles.scss';
+</style>
