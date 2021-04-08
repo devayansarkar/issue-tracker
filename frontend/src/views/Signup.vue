4cfa21d251d637b932951a9e7c74af1e9ef5ad07
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-08 22:20:10 +0200

    adding sign up form and adding message in case the passwords do not match

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -2,51 +2,72 @@
   <div class="auth-form-container">
     <div class="left-section">
       <div class="login-form-container">
+        <div v-if="message.text" class="info-message" :class="message.type">
+            {{ message.text }}
+          </div>
         <div class="app-title">Issue Tracker</div>
         <div class="app-subtitle">Sign up</div>
-        <div class="input-container">
-          <label for="name" class="form-field-label">Name</label>
-          <input
-            id="name"
-            type="text"
-            class="form-field"
-            placeholder="Enter your name"
-          />
-        </div>
-        <div class="input-container">
-          <label for="email" class="form-field-label">Email</label>
-          <input
-            id="email"
-            type="text"
-            class="form-field"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div class="input-container">
-          <label for="password" class="form-field-label">Password</label>
-          <input
-            id="password"
-            type="password"
-            class="form-field"
-            placeholder="Enter your password"
-          />
-        </div>
-        <div class="input-container">
-          <label for="password-confirmation" class="form-field-label"
-            >Retype password</label
-          >
-          <input
-            id="password-confirmation"
-            type="password"
-            class="form-field"
-            placeholder="Enter your password again"
-          />
-        </div>
-        <button class="btn btn-primary">Sign up</button>
+        <form @submit.prevent="signup">
+          <div class="input-container">
+            <label for="name" class="form-field-label"
+              >Name <span class="mandatory">*</span></label
+            >
+            <input
+              id="name"
+              type="text"
+              v-model="name"
+              class="form-field"
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div class="input-container">
+            <label for="email" class="form-field-label"
+              >Email <span class="mandatory">*</span></label
+            >
+            <input
+              id="email"
+              type="email"
+              v-model="email"
+              class="form-field"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div class="input-container">
+            <label for="password" class="form-field-label"
+              >Password <span class="mandatory">*</span></label
+            >
+            <input
+              id="password"
+              type="password"
+              v-model="password"
+              class="form-field"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div class="input-container">
+            <label for="password-confirmation" class="form-field-label"
+              >Retype password <span class="mandatory">*</span></label
+            >
+            <input
+              id="password-confirmation"
+              type="password"
+              v-model="password_confirmation"
+              class="form-field"
+              placeholder="Enter your password again"
+              required
+            />
+          </div>
+          <button class="btn btn-primary" :disabled="!isFormValid()">
+            Sign up
+          </button>
+        </form>
         <div class="sign-up-info">Already have an account ?</div>
-        <router-link to="/login"
-          ><button class="btn btn-secondary">Go to login</button></router-link
-        >
+        <router-link to="/">
+          <button class="btn btn-secondary">Go to login</button>
+        </router-link>
       </div>
     </div>
     <div class="right-section"></div>
@@ -56,6 +77,37 @@
 <script>
 export default {
   name: 'SignUp',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      message: {
+        type: 'success',
+        text: '',
+      },
+    };
+  },
+  methods: {
+    signup() {
+      if (this.password !== this.password_confirmation) {
+        this.message = {
+          type: 'error',
+          text: 'Password confirmation do not match!',
+        };
+      }
+    },
+    isFormValid() {
+      if (this.name === '' || this.name === undefined) return false;
+      if (this.email === '' || this.email === undefined) return false;
+      if (this.password === '' || this.password === undefined) return false;
+      if (
+        this.password_confirmation === '' || this.password_confirmation === undefined
+      ) return false;
+      return true;
+    },
+  },
 };
 </script>
 
@@ -64,4 +116,5 @@ export default {
 @import '../styles/button.styles.scss';
 @import '../styles/input.styles.scss';
 @import '../styles/signup.page.styles.scss';
+@import '../styles/info.styles.scss';
 </style>
