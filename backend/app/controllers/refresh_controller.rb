de5c15ec300af73774bb5c956ad282e64dfd6a89
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-08 20:06:36 +0200

    allowing refresh..before expiry

--- backend/app/controllers/refresh_controller.rb
+++ backend/app/controllers/refresh_controller.rb
@@ -3,10 +3,7 @@ class RefreshController < ApplicationController
 
   def create
     session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
-    tokens = session.refresh_by_access_payload do
-       render json:{ error: 'Refresh action is performed before the expiration of the access token.'}, status: :bad_request
-       return
-      end
+    tokens = session.refresh_by_access_payload 
     response.set_cookie(JWTSessions.access_cookie,
                         value: tokens[:access],
                         httponly: true,
