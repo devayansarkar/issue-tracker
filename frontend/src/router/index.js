296cba26c9464c7d57f20aaeb8039eb6dd484f1b
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-08 21:39:05 +0200

    except login all other views are lazy loaded

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 const routes = [
   {
     path: '/home',
     name: 'Home',
-    component: Home,
+    component: () => import('../views/Home.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/',
-    name: 'Login',
-    component: () => import('../views/Login.vue'),
+    component: () => import('../views/About.vue'),
   },
   {
     path: '/signup',
     name: 'SignUp',
     component: () => import('../views/Signup.vue'),
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
   },
 ];
 
