09edf0cca8a53eb0e661b3b34d00894bf269faed
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-09 23:17:35 +0200

    user in api/v1 routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
         namespace :v1 do
           resources :issues
           resources :comments
+          get 'user', controller: :users, action: :index
         end
     end
     
     post 'refresh', controller: :refresh, action: :create
     post 'signin', controller: :signin, action: :create
     post 'signup', controller: :signup, action: :create
-    delete 'signin',controller: :signin, action: :destroy
+    delete 'signin', controller: :signin, action: :destroy
 
     # Only get requests return the html for the frontend
     match '*path' => redirect(path: '/'), via: :get
