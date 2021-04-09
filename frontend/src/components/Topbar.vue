3db4ae37730b96475a3ab08635fc1b80d57d6040
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-09 09:35:36 +0200

    adding base topbar

---
+++ frontend/src/components/Topbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="topbar">
+    <div class="app-name">Issue tracker</div>
+    <div class="profile-container">
+      <div class="profile">
+        <div class="name">Devayan Kumar Sarkar</div>
+        <div class="email">DevayanSarkar@gmail.com</div>
+      </div>
+      <div class="icon">
+        <span class="material-icons"> account_circle </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topbar',
+};
+</script>
+
+<style lang="scss">
+@import '../styles/topbar.styles.scss';
+</style>
