adb56238a21a50272d93b8831431233eff33e49e
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-09 09:36:46 +0200

    adding top bar for info

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
     <Navbar />
+    <div class="page-container">
+      <Topbar />
+      <p>Hello from the otherside!!!</p>
+    </div>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Topbar from '@/components/Topbar.vue';
 
 export default {
   name: 'Home',
   components: {
     Navbar,
+    Topbar,
   },
 };
 </script>
+
+<style lang="scss">
+@import '../styles/home.page.styles.scss';
+</style>
