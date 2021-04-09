676343b85e97db1ba13e4c3190361c71282f33c7
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-09 23:17:16 +0200

    validating the status of an issue

--- backend/app/models/issue.rb
+++ backend/app/models/issue.rb
@@ -1,4 +1,6 @@
 class Issue < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :status, inclusion: { in: %w(TODO INPROGRESS DONE), message: "%{value} is not a valid status for an issue. Must be one of these values: [TODO, INPROGRESS and DONE], in upper case." }
+
 end
