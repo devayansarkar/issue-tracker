301cbe6323d9b0354729eda339f03369e0661471
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-09 23:30:28 +0200

    returning user information for the home page

---
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+module Api
+    module V1
+        class UsersController < ApplicationController
+            before_action :authorize_access_request!
+
+            def index
+                render json: {
+                    name: current_user['name'],
+                    email: current_user['email'],
+                    deadlineIssues: Issue.where(user_id: current_user['id']).order(end_date: :asc).limit(4),
+                    recentlyUpdatedIssues: Issue.where(user_id: current_user['id']).order(updated_at: :desc).limit(4),
+                    finished: Issue.where(status: 'DONE', user_id: current_user['id']).count,
+                    todo: Issue.where(status: 'TODO', user_id: current_user['id']).count,
+                    inProgress: Issue.where(status: 'INPROGRESS', user_id: current_user['id']).count,
+                }
+            end
+            
+        end 
+    end
+end
