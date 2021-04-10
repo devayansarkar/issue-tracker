4deaa8ed4df3389fecf5d3acb4c046d723970005
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-10 22:11:33 +0200

    hooking up states for name and email

--- frontend/src/components/Topbar.vue
+++ frontend/src/components/Topbar.vue
@@ -7,8 +7,8 @@
     </div>
     <div class="profile-container">
       <div class="profile">
-        <div class="name">Devayan Kumar Sarkar</div>
-        <div class="email">DevayanSarkar@gmail.com</div>
+        <div class="name">{{ $store.getters.getUser.name }}</div>
+        <div class="email">{{ $store.getters.getUser.email }}</div>
       </div>
       <div class="icon">
         <span class="material-icons"> account_circle </span>
