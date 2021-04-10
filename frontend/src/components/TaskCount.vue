81e66bf43acfcc32f3e186bfa855736896c675c9
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-10 12:23:04 +0200

    splitting into separate components

---
+++ frontend/src/components/TaskCount.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="task-count" :class="cardType == 'total-card' ? 'total' : 'task-count'">
+    <div>
+      <div class="count">{{ count }}</div>
+      <div class="type">{{ type }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskCount',
+  props: { cardType: String, count: Number, type: String },
+};
+</script>
+
+<style lang="scss">
+@import '../styles/task.count.styles.scss';
+</style>
