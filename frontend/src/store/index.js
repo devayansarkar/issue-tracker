2293f4ce51166c53492b04180066fdbcaea47b5f
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-10 22:11:17 +0200

    Creating initial state objects

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,35 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    user: {
+      name: 'Devayan',
+      email: 'devayan@gmail.com',
+    },
+    deadlineItems: [],
+    recentItem: [],
+    taskCount: {
+      inprogress: 12,
+      done: 6,
+      doing: 6,
+    },
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    getUser(state) {
+      return { ...state.user };
+    },
+    // getRecentItems(state) { },
+    // getDeadlineItems(state) { },
+    getTaskCount(state) {
+      return {
+        ...state.taskCount,
+        total: state.taskCount.doing + state.taskCount.done + state.taskCount.inprogress,
+      };
+    },
   },
 });
