bc5e196f9f873ee8b9fff27ada80ee5475f55235
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-10 22:46:01 +0200

    mutating user tasks based on the response received

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,22 +1,51 @@
 import { createStore } from 'vuex';
+import { securedConnection } from '../config/axios';
 
 export default createStore({
   state: {
+    isLoading: true,
     user: {
-      name: 'Devayan',
-      email: 'devayan@gmail.com',
+      name: '',
+      email: '',
     },
     deadlineItems: [],
     recentItem: [],
     taskCount: {
-      inprogress: 12,
-      done: 6,
-      doing: 6,
+      inprogress: 0,
+      done: 0,
+      todo: 0,
+    },
+    error: {
+      isError: false,
+      message: '',
     },
   },
   mutations: {
+    initiateLoadUserInfo(state) {
+      state.isLoading = true;
+    },
+    loadUserInfoSuccess(state, responseBody) {
+      state.user = { name: responseBody.name, email: responseBody.email };
+      state.taskCount = {
+        done: responseBody.done,
+        inprogress: responseBody.inProgress,
+        todo: responseBody.todo,
+      };
+      state.isLoading = false;
+    },
+    loadUserInfoFailure(state) {
+      state.isLoading = false;
+    },
   },
   actions: {
+    loadUserInfo({ commit }) {
+      commit('initiateLoadUserInfo');
+      securedConnection
+        .get('/api/v1/user')
+        .then((r) => commit('loadUserInfoSuccess', r.data))
+        .catch((e) => commit('loadUserInfoFailure', e));
+      console.log('Loading user info (action)');
+    },
   },
   modules: {
   },
@@ -29,7 +58,7 @@ export default createStore({
     getTaskCount(state) {
       return {
         ...state.taskCount,
-        total: state.taskCount.doing + state.taskCount.done + state.taskCount.inprogress,
+        total: state.taskCount.todo + state.taskCount.done + state.taskCount.inprogress,
       };
     },
   },
