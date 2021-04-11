5fb6124dfb2bf95802bbb8573b2fc118ab5011dc
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-11 08:13:01 +0200

    creating a component for task status update

---
+++ frontend/src/components/TaskStatusInfoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="task update">
+    <div class="info">
+      <div class="icon">
+        <span class="material-icons"> thumb_up </span>
+      </div>
+      <div class="meesage">{{getMessage}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskStatusInfoCard',
+  props: ['cardType'],
+  computed: {
+    getMessage() {
+      if (this.cardType === 'deadline') {
+        return 'No deadline tasks.';
+      }
+      return 'No issues recently updated.';
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../styles/task.card.styles';
+</style>
