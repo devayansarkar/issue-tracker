1b337f8a1938b856240cc10c3cae2a25feaa1348
Devayan Sarkar <devayansarkar@gmail.com>
2021-04-27 00:37:53 +0200

    skeleton in mobile view for add issue

---
+++ frontend/src/views/Issue.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <Navbar :page="'addIssue'" />
+    <div class="page-container">
+      <Topbar />
+      <div class="add-issue">
+        <div class="page-type">{{ getPageTitle }}</div>
+        <div class="issue">
+          <form @submit.prevent="saveIssue">
+            <div class="input-container">
+              <label for="title" class="form-field-label">Title</label>
+              <input
+                id="title"
+                type="text"
+                v-model="title"
+                class="form-field"
+                placeholder="Enter issue title"
+              />
+            </div>
+            <div class="input-container">
+              <label for="description" class="form-field-label"
+                >Description</label
+              >
+              <textarea
+                id="description"
+                v-model="description"
+                type="description"
+                placeholder="Add your description here"
+                cols="30"
+                rows="10"
+              ></textarea>
+            </div>
+            <div class="issue-info">
+              <div class="input-container">
+                <label for="date" class="form-field-label">End date</label>
+                <input
+                  id="date"
+                  type="date"
+                  v-model="endDate"
+                  class="form-field"
+                  placeholder="Enter issue date"
+                />
+              </div>
+              <div class="input-container">
+                <label for="lane" class="form-field-label">Add to</label>
+                <input
+                  id="lane"
+                  type="dropdown"
+                  v-model="lane"
+                  class="form-field"
+                  placeholder="Add your lane"
+                />
+              </div>
+              <div class="input-container">
+                <label for="category" class="form-field-label">Category</label>
+                <input
+                  id="category"
+                  type="text"
+                  v-model="category"
+                  class="form-field"
+                  placeholder="Enter issue category"
+                />
+              </div>
+            </div>
+            <div class="issue-cta">
+              <button :disabled="isFormIncomplete" class="btn btn-primary">
+                {{ getButtonName }}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Topbar from '@/components/Topbar.vue';
+
+export default {
+  name: 'AddNewIssue',
+  components: {
+    Navbar,
+    Topbar,
+  },
+  data() {
+    return {
+      title: '',
+      description: '',
+      endDate: '',
+      lane: 'TODO',
+      category: '',
+      pageType: this.$route.name,
+    };
+  },
+  computed: {
+    isFormIncomplete() {
+      return this.title === '' || this.description === '';
+    },
+    getButtonName() {
+      if (this.pageType === 'AddIssue') {
+        return 'Save';
+      }
+      return 'Update';
+    },
+    getPageTitle() {
+      if (this.pageType === 'AddIssue') {
+        return 'Add issue';
+      }
+      return 'Update issue';
+    },
+  },
+  methods: {
+    saveIssue() {
+      console.log('Saving issue');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../styles/issue.styles.scss';
+</style>
