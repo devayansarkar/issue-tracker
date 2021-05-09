9168d5f224f78c96e4ceb8168984a5d08f1cb255
Devayan Sarkar <devayansarkar@gmail.com>
2021-05-09 21:31:12 +0200

    Adding about page skeleton

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <Navbar :page="'home'" />
+    <div class="page-container">
+      <Topbar />
+    </div>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Topbar from '@/components/Topbar.vue';
+
+export default {
+  name: 'About',
+  components: {
+    Navbar,
+    Topbar,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../styles/about.page.styles.scss';
+</style>
