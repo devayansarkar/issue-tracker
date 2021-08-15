7fc0c9d6d2b484169f740bf070a6592806ec8483
Devayan Sarkar <devayansarkar@gmail.com>
2021-08-15 15:32:53 +0200

    styling the profile page

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,40 +1,46 @@
 <template>
-  <div class="profile-page">
-    <div class="profile-page-container">
-      <div class="profile-heading">My profile</div>
-      <div class="profile">
-        <div class="user-name">{{ $store.getters.getUser.name }}</div>
-        <div class="user-email">{{ $store.getters.getUser.email }}</div>
-        <div class="issue-status">
-          <div class="issue-field">
-            <div class="issue-title">Total</div>
-            <div class="issue-count">
-              {{ $store.getters.getTaskCount.total }}
+  <div>
+    <Navbar :page="'profile'" />
+    <div class="page-container">
+      <Topbar />
+      <div class="profile-page">
+        <div class="profile-page-container">
+          <div class="profile-heading">My profile</div>
+          <div class="profile">
+            <div class="user-name">{{ $store.getters.getUser.name }}</div>
+            <div class="user-email">{{ $store.getters.getUser.email }}</div>
+            <div class="issue-status">
+              <div class="issue-field">
+                <div class="issue-title">Total</div>
+                <div class="issue-count">
+                  {{ $store.getters.getTaskCount.total }}
+                </div>
+              </div>
+              <div class="issue-field">
+                <div class="issue-title">Todo</div>
+                <div class="issue-count">
+                  {{ $store.getters.getTaskCount.todo }}
+                </div>
+              </div>
+              <div class="issue-field">
+                <div class="issue-title">Doing</div>
+                <div class="issue-count">
+                  {{ $store.getters.getTaskCount.doing }}
+                </div>
+              </div>
+              <div class="issue-field">
+                <div class="issue-title">Done</div>
+                <div class="issue-count">
+                  {{ $store.getters.getTaskCount.total }}
+                </div>
+              </div>
+              <div class="cta">
+                <router-link class="home-navigation" to="/">
+                  Go back to home
+                </router-link>
+                <span class="logout" @click="logout"> Logout </span>
+              </div>
             </div>
-          </div>
-          <div class="issue-field">
-            <div class="issue-title">Todo</div>
-            <div class="issue-count">
-              {{ $store.getters.getTaskCount.todo }}
-            </div>
-          </div>
-          <div class="issue-field">
-            <div class="issue-title">Doing</div>
-            <div class="issue-count">
-              {{ $store.getters.getTaskCount.doing }}
-            </div>
-          </div>
-          <div class="issue-field">
-            <div class="issue-title">Done</div>
-            <div class="issue-count">
-              {{ $store.getters.getTaskCount.total }}
-            </div>
-          </div>
-          <div class="cta">
-            <router-link class="home-navigation" to="/">
-              Go back to home
-            </router-link>
-            <span class="logout" @click="logout"> Logout </span>
           </div>
         </div>
       </div>
@@ -43,8 +49,15 @@
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue';
+import Topbar from '@/components/Topbar.vue';
+
 export default {
   name: 'Profile',
+  components: {
+    Navbar,
+    Topbar,
+  },
   methods: {
     logout() {
       this.$store.dispatch('logout');
