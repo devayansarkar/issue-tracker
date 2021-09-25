152065cfd3f169cdf948f63378020c4458628871
Devayan Sarkar <devayansarkar@gmail.com>
2021-09-25 22:50:26 +0200

    added a new route for orders

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
           resources :issues
+          resources :order
           resources :comments
           get 'user', controller: :users, action: :index
         end
