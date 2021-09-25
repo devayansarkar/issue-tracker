58740947c623f1f9a8fba293819016f3d9257538
Devayan Sarkar <devayansarkar@gmail.com>
2021-09-25 22:51:02 +0200

    Handling missing parameter exception

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
     include JWTSessions::RailsAuthorization
     rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
-
+    rescue_from ActionController::ParameterMissing, with: :missing_parameters
+    
     def action_not_found
         render json:{error: 'The resource you are requesting is not available.'}, status: :not_found
     end
@@ -14,6 +15,10 @@ class ApplicationController < ActionController::API
 
     def not_authorized
         render json:{error: 'Access denied. You must be logged in to access this resource.'}, status: :unauthorized
+    end
+
+    def missing_parameters(exception)
+        render json:{error: exception.message}, status: :bad_request
     end
 
 end
