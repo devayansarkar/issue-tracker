afdfd45dda7cdece5766aadb604b2fe1e0b3d892
Devayan Sarkar <devayansarkar@gmail.com>
2021-09-27 19:53:51 +0200

    updating the db with the new order

--- backend/app/controllers/api/v1/order_controller.rb
+++ backend/app/controllers/api/v1/order_controller.rb
@@ -9,31 +9,32 @@ module Api
             # PATCH /i/1
             # Updates the exisiting issue
             def update
-                # For the issue's destination lane, find the issue, whose next node is the issue's next node
-                # point the issue to the current one, and the current one's points tp the next node
-                # In case the current issue's next node is `nil`, it goes to the end and the current's lane's nill points to the current issue
-                # In case the current issue is the first, then there is no issue in the lane that points to an issue, until now.
-                
-                preceding_issue = Issue.where(next_issue: @issue.id, status: @issue.status)
+
+                target_next_issue  = issue_params().fetch(:next_issue)
+                target_status = issue_params().fetch(:status)
+                target_next_issue = nil if target_next_issue == -1
             
+                preceding_issue = current_user.issues.where(next_issue: @issue.id, status: @issue.status)
+                
+
                 # For the old lane
                 if preceding_issue.length > 0  then
                     next_issue = nil
                     if !@issue.next_issue.nil? then
-                        next_issue_in_lane = Issue.where(next_issue: @issue.next_issue, status: @issue.status)
-                        next_issue = next_issue_in_lane.id
+                        next_issue_in_lane = current_user.issues.where(next_issue: @issue.next_issue, status: @issue.status)
+                        next_issue = next_issue_in_lane[0].id
                     end
                     preceding_issue[0].update(next_issue: next_issue)
                 end
                 
-                destination_preceding_issue = Issue.where(next_issue: issue_params().fetch(:next_issue), status: issue_params().fetch(:status))
+                destination_preceding_issue = current_user.issues.where(next_issue: target_next_issue, status: target_status)
                 next_issue = nil
                 
                 if destination_preceding_issue.length > 0 then 
                     destination_preceding_issue[0].update(next_issue: @issue.id)
                 end
 
-                @issue.update(next_issue: issue_params().fetch(:next_issue), status: issue_params().fetch(:status))
+                @issue.update(next_issue: target_next_issue, status: target_status)
                 
                 render json: @issue
             end
