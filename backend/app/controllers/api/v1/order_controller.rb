a2c78bcadcd3003aca166aac63ed345486159c31
Devayan Sarkar <devayansarkar@gmail.com>
2021-09-28 22:00:37 +0200

    keeping the order in th db

--- backend/app/controllers/api/v1/order_controller.rb
+++ backend/app/controllers/api/v1/order_controller.rb
@@ -9,33 +9,28 @@ module Api
             # PATCH /i/1
             # Updates the exisiting issue
             def update
-
                 target_next_issue  = issue_params().fetch(:next_issue)
                 target_status = issue_params().fetch(:status)
                 target_next_issue = nil if target_next_issue == -1
-            
+
                 preceding_issue = current_user.issues.where(next_issue: @issue.id, status: @issue.status)
-                
 
                 # For the old lane
                 if preceding_issue.length > 0  then
                     next_issue = nil
                     if !@issue.next_issue.nil? then
-                        next_issue_in_lane = current_user.issues.where(next_issue: @issue.next_issue, status: @issue.status)
-                        next_issue = next_issue_in_lane[0].id
+                        next_issue = @issue.next_issue
                     end
                     preceding_issue[0].update(next_issue: next_issue)
                 end
                 
                 destination_preceding_issue = current_user.issues.where(next_issue: target_next_issue, status: target_status)
-                next_issue = nil
                 
                 if destination_preceding_issue.length > 0 then 
                     destination_preceding_issue[0].update(next_issue: @issue.id)
                 end
 
                 @issue.update(next_issue: target_next_issue, status: target_status)
-                
                 render json: @issue
             end
 
