ddead11419142dc88301745b8f707491f3eee8ff
Devayan Sarkar <devayansarkar@gmail.com>
2021-09-29 16:08:32 +0200

    mapping issue response for user controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -4,11 +4,48 @@ module Api
             before_action :authorize_access_request!
 
             def index
+                done_issues = Issue.where(user_id: current_user.id).where.not(status: 'DONE').order(end_date: :asc).limit(4)
+                recently_updated_issues = Issue.where(user_id: current_user.id).order(updated_at: :desc).limit(4)
+
+                done_issues_response = Array.new
+                recently_updated_issues_response = Array.new
+
+                done_issues.each { |item|
+                    done_issues_response.push(
+                        {
+                            "id": item[:issue_number],
+                            "title": item[:title],
+                            "description": item[:description],
+                            "end_date":  item[:end_date],
+                            "category": item[:category],
+                            "status":  item[:status],
+                            "user_id":  item[:user_id],
+                            "created_at":  item[:created_at],
+                            "updated_at":  item[:updated_at],
+                        }
+                    )
+                }
+
+                recently_updated_issues.each { |item|
+                    recently_updated_issues_response.push(
+                        {
+                            "id": item[:issue_number],
+                            "title": item[:title],
+                            "description": item[:description],
+                            "end_date":  item[:end_date],
+                            "category": item[:category],
+                            "status":  item[:status],
+                            "user_id":  item[:user_id],
+                            "created_at":  item[:created_at],
+                            "updated_at":  item[:updated_at],
+                        }
+                    )
+                }
                 render json: {
                     name: current_user.name,
                     email: current_user.email,
-                    deadlineIssues: Issue.where(user_id: current_user.id).where.not(status: 'DONE').order(end_date: :asc).limit(4),
-                    recentlyUpdatedIssues: Issue.where(user_id: current_user.id).order(updated_at: :desc).limit(4),
+                    deadlineIssues: done_issues_response,
+                    recentlyUpdatedIssues: recently_updated_issues_response,
                     done: Issue.where(status: 'DONE', user_id: current_user.id).count,
                     todo: Issue.where(status: 'TODO', user_id: current_user.id).count,
                     inProgress: Issue.where(status: 'INPROGRESS', user_id: current_user.id).count,
