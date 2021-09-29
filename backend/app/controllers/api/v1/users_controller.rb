ffaf7e879f418afbb66997d35e0f734ec9b9b1b0
Devayan Sarkar <devayansarkar@gmail.com>
2021-09-29 16:03:53 +0200

    using object notation

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -5,13 +5,13 @@ module Api
 
             def index
                 render json: {
-                    name: current_user['name'],
-                    email: current_user['email'],
-                    deadlineIssues: Issue.where(user_id: current_user['id']).where.not(status: 'DONE').order(end_date: :asc).limit(4),
-                    recentlyUpdatedIssues: Issue.where(user_id: current_user['id']).order(updated_at: :desc).limit(4),
-                    done: Issue.where(status: 'DONE', user_id: current_user['id']).count,
-                    todo: Issue.where(status: 'TODO', user_id: current_user['id']).count,
-                    inProgress: Issue.where(status: 'INPROGRESS', user_id: current_user['id']).count,
+                    name: current_user.name,
+                    email: current_user.email,
+                    deadlineIssues: Issue.where(user_id: current_user.id).where.not(status: 'DONE').order(end_date: :asc).limit(4),
+                    recentlyUpdatedIssues: Issue.where(user_id: current_user.id).order(updated_at: :desc).limit(4),
+                    done: Issue.where(status: 'DONE', user_id: current_user.id).count,
+                    todo: Issue.where(status: 'TODO', user_id: current_user.id).count,
+                    inProgress: Issue.where(status: 'INPROGRESS', user_id: current_user.id).count,
                 }
             end
             
