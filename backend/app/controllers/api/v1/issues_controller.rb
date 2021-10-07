9aa3f1d2f4bd8e50422484e76a1f0588e62a4d2b
Devayan Sarkar <devayansarkar@gmail.com>
2021-10-08 00:26:32 +0200

    incrementing the correct way

--- backend/app/controllers/api/v1/issues_controller.rb
+++ backend/app/controllers/api/v1/issues_controller.rb
@@ -56,7 +56,7 @@ module Api
                 
                 issue_counter = IssueCounter.find_by(user_id: current_user.id)
                 @issue.issue_number = issue_counter.next_issue_number
-                issue_counter.increment!(next_issue_number)
+                issue_counter.increment!(:next_issue_number)
 
                 preceding_issue = Issue.where(next_issue: nil, status: @issue.status, user_id: current_user.id)
                 if preceding_issue[0].nil? then 
