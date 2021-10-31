fa8890bca7590213a763161c7aec5e9a8469717d
Devayan Sarkar <devayansarkar@gmail.com>
2021-10-31 22:31:28 +0100

    correct test names

--- e2e/cypress/integration/1-signin/login.spec.js
+++ e2e/cypress/integration/1-signin/login.spec.js
@@ -6,7 +6,7 @@ describe('Validate log in feature works', () => {
         cy.visit(Cypress.env('HOST'))
     })
 
-    it('Make sure log in works with correct username and password', () => {
+    it('Log in works with correct username and password', () => {
         cy.get('[data-cy=email]').type(Cypress.env('EMAIL'))
         cy.get('[data-cy=password]').type(Cypress.env('PASSWORD'))
         cy.get('[data-cy=login]').first().click()
@@ -14,7 +14,7 @@ describe('Validate log in feature works', () => {
         cy.get('[data-cy=home-title]').contains('Welcome to your personal issue tracker')
     })
 
-    it('Make sure log in fails with incorrect username and password', () => {
+    it('Log in fails with incorrect username and password', () => {
         cy.get('[data-cy=email]').type('my@email.com')
         cy.get('[data-cy=password]').type('unknownpassword')
         cy.get('[data-cy=login]').first().click()
