ec31b7c030df381c5be03c2d2845d36586373994
Devayan Sarkar <devayansarkar@gmail.com>
2021-11-01 18:55:34 +0100

    updating the test

--- e2e/cypress/integration/3-viewAndUpdateIssue/viewAndUpdateIssue.spec.js
+++ e2e/cypress/integration/3-viewAndUpdateIssue/viewAndUpdateIssue.spec.js
@@ -16,7 +16,7 @@ describe('Validate viewing and updating an issue works', () => {
         cy.get('[data-cy=password-input]').type(Cypress.env('PASSWORD'))
         cy.get('[data-cy=login-button]').first().click()
         cy.wait(500)
-        cy.visit(`${Cypress.env('HOST')}/view-issue/22`)
+        cy.visit(`${Cypress.env('HOST')}/view-issue/5`)
         cy.get('[data-cy=title-input]').click().clear().type(`This is the title - ${todayDateString}`)
         cy.get('[data-cy=description-input]').clear().type(`This is the description - ${todayDateString}`)
         cy.get('[data-cy=lane-input]').select('INPROGRESS')
@@ -30,5 +30,11 @@ describe('Validate viewing and updating an issue works', () => {
         cy.get('[data-cy=lane-input]').should('have.value', 'INPROGRESS')
         cy.get('[data-cy=category-input]').should('have.value', 'CI-TEST-UPDATE')
         cy.get('[data-cy=date-input]').should('have.value', dateInput)
+        cy.get('[data-cy=all-issues-navigation-button]').click()
+        // So that issue is added to the board, without refresh
+        cy.contains(`This is the title - ${todayDateString}`)
+        cy.reload()
+        // So that issue is returned from the back and is still present after refresh
+        cy.contains(`This is the title - ${todayDateString}`)
     })
 })
