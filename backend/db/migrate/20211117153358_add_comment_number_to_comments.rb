fd1a182434d3cb3041837c654d8ec165a0d9fbaa
Devayan Sarkar <devayansarkar@gmail.com>
2021-11-17 18:45:51 +0100

    correct the schema

--- backend/db/migrate/20211117153358_add_comment_number_to_comments.rb
+++ backend/db/migrate/20211117153358_add_comment_number_to_comments.rb
@@ -3,7 +3,7 @@ class AddCommentNumberToComments < ActiveRecord::Migration[6.1]
     add_column :comments, :comment_number, :integer
     create_table :comment_counters do |t|
       t.integer :next_comment_number
-      t.references :issues, null: false, foreign_key: true
+      t.references :issue, null: false, foreign_key: true
       t.timestamps
     end
     add_index :comments, [:comment_number, :issue_id, :user_id], unique: true, name: 'unique_identifier_with_user_issue_and_comment_number'
